8c845eb57fac5b3aa7947b4e12b58880fd575252
RandyForte <forterandy@outlook.com>
2017-10-05 21:34:14 -0400

    Fixed the printing of the loan details in the customer class
    Calling the toString method was not printing the loan details

--- src/Customer.java
+++ src/Customer.java
@@ -80,7 +80,7 @@ class Customer extends User {
 		System.out.println(loanList.size());
 		for (Loan loan : loanList) {
 			if (loan.status == "active")
-				loan.toString();
+				System.out.println(loan);
 		}
 	}
 
@@ -164,7 +164,7 @@ class Customer extends User {
 			if (loanList.add(temp)) {
 				Bank.setTotalMoneyLent(Bank.getTotalMoneyLent() + amount);
 				System.out.println("Your loan is issued");
-				temp.toString();
+				System.out.println(temp);
 			}
 		}
 		if (type.equals("EducationLoan")) {
@@ -172,7 +172,7 @@ class Customer extends User {
 			if (loanList.add(temp)) {
 				Bank.setTotalMoneyLent(Bank.getTotalMoneyLent() + amount);
 				System.out.println("Your loan is issued");
-				temp.toString();
+				System.out.println(temp);
 			}
 		}
 	}
